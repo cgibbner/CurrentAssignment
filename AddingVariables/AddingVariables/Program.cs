47826a49da7571e8fc03d96d720d99931f052909
Cody Gibbner <cgibbner@aerialcom.net>
2018-05-08 16:44:33 -0400

    Main structure built

--- AddingVariables/AddingVariables/Program.cs
+++ AddingVariables/AddingVariables/Program.cs
@@ -10,6 +10,38 @@ namespace AddingVariables
     {
         static void Main(string[] args)
         {
+            string name;
+            int zip;
+            double salary;
+
+            Console.WriteLine("");
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine("Hex Is Best v1.0");
+            Console.WriteLine("");
+
+            Console.WriteLine("Thank you for choosing the Hexagon Technologies - Hex Is Best - software.");
+            Console.WriteLine("The industry standard for pre-program launch welcome messages.");
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine("");
+
+            Console.WriteLine("Please enter software developer's name:");
+            Console.Write(" --> ");
+
+            name = Console.ReadLine();
+
+            Console.WriteLine("Please enter software developers ZIP code:");
+            Console.Write(" --> ");
+
+            zip = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter software developers monthly gross pay:");
+            Console.Write(" --> ");
+
+            salary = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(name);
+            Console.WriteLine(zip);
+            Console.WriteLine(salary);
         }
     }
 }
