8f39f92f8f9e025b472ac99fa96cfe6110176f68
Cody Gibbner <cgibbner@yahoo.com>
2018-10-04 16:04:09 -0400

    Update 10-4 1604.

---
+++ Arialcom/js/javas.js
@@ -0,0 +1,11 @@
+function changetxtcolor() {
+    
+    var text = document.getElementById("test").style.color = "red";
+    
+}
+
+function changetxtcolorbk() {
+    
+    var text = document.getElementById("test").style.color = "";
+    
+}
